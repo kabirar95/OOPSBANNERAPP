d84ba2afc32c871ebbecd0f696b1366e3f7bba03
kabirar95 <kabir.ramteke8@gmail.com>
2026-03-13 14:43:56 +0530

    Second usecase

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,3 +1,12 @@
-public static void main(String[] args) {
-    System.out.println("Welcome to the OOPs Banner Application!");
+public class OOPSBannerApp {
+    public static void main(String[] args){
+        System.out.println("   *******     *******   *******   ********");
+        System.out.println("  **     **   **     **  **    ** **       ");
+        System.out.println(" **       ** **       ** **    ** **       ");
+        System.out.println(" **       ** **       ** *******  *********");
+        System.out.println(" **       ** **       ** **              **");
+        System.out.println("  **     **   **     **  **              **");
+        System.out.println("   *******     *******   **       *********");
+    }
+    
 }
